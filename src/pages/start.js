5ce612e086b1f7269fafd9445abc328b3e061669
Carms Ng <contact@carms-ng.com>
2021-06-24 19:20:12 -0400

    [WIP] start page - added graphql query

--- src/pages/start.js
+++ src/pages/start.js
@@ -1,16 +1,56 @@
 import React from "react"
+import { graphql } from "gatsby"
 import Layout from "../components/Layout"
 import Seo from "../components/Seo"
 import SiteBorderStyles from "../styles/SiteBorderStyles"
+import Hero from '../components/Hero'
 
-export default function GetStartedPage() {
+export default function GetStartedPage({ data: { page } }) {
+  const { title, hero, questions } = page.childMarkdownRemark.frontmatter
+  console.log(title, hero, questions)
   return (
     <Layout>
-      <Seo title="Get Started" />
+      <Seo title={title} />
       <SiteBorderStyles>
-        <h1>Start</h1>
+        <Hero heading={hero.heading} description={hero.description} image={hero.image} />
       </SiteBorderStyles>
     </Layout>
   )
 }
 
+export const data = graphql`
+  query {
+    page: file(relativeDirectory: {eq: "pages"}, name: {eq: "start"}) {
+      childMarkdownRemark {
+        frontmatter {
+          title
+          hero {
+            heading
+            description
+            image {
+              alt
+              imageFile {
+                childImageSharp {
+                  gatsbyImageData(
+                    width: 400
+                    placeholder: BLURRED
+                    layout: CONSTRAINED
+                  )
+                }
+              }
+            }
+          }
+          questions {
+            question
+            description
+            selectLabel
+            options {
+              option
+              response
+            }
+          }
+        }
+      }
+    }
+  }
+`
